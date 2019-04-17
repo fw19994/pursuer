bf0573fde22f51fc897eecd9b62b9256170b5275
马艳涛 <mayantao1996@126.com>
2019-04-17 21:52:55 +0800

    初始化项目

---
+++ pursuer-business/pursuer-business-display/pursuer-business-userinfo/pursuer-business-userinfo-service/src/test/java/com/pursuer/microservice/userinfo/PursuerBusinessDisplayUserinfoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pursuer.microservice.userinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PursuerBusinessDisplayUserinfoAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
