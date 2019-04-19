61f87a73ada4aad59d1c013930a1c1cb80816d7a
马艳涛 <mayantao1996@126.com>
2019-04-19 20:58:09 +0800

    注册中心修改

--- pursuer-gateway/pursuer-gateway-discovery/src/main/java/com/pursuer/microservice/discovery/PursuerGatewayDiscoveryApplication.java
+++ pursuer-gateway/pursuer-gateway-discovery/src/main/java/com/pursuer/microservice/discovery/PursuerGatewayDiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.pursuer.microservice.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class PursuerGatewayDiscoveryApplication {
 
