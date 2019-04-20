75bb38acfd48eeb24bf3517df9ca3341d77d2863
184343665872163.com <fengwei1994>
2019-04-20 16:55:20 +0800

    配置文件pom修改-冯伟2019-04-17

--- pursuer-business/pursuer-business-display/pursuer-business-orderinfo/pursuer-business-orderinfo-app/src/main/java/com/PursuerBusinessDisplayOrderinfoApplication.java
+++ pursuer-business/pursuer-business-display/pursuer-business-orderinfo/pursuer-business-orderinfo-app/src/main/java/com/PursuerBusinessDisplayOrderinfoApplication.java
@@ -1,11 +1,13 @@
-package com.pursuer.microservice.orderinfo;
+package com;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@MapperScan("com.pursuer.microservice.orderinfo.app.dao")
 public class PursuerBusinessDisplayOrderinfoApplication {
 
     public static void main(String[] args) {
